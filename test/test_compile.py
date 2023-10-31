977078505ee4e06d2f494275fe2310a67fb0a3dc
drisspg <drisspguessous@gmail.com>
2023-10-31 10:21:50 -0700

    guard compile tests

--- test/test_compile.py
+++ test/test_compile.py
@@ -2,6 +2,7 @@ import copy
 import random
 
 import pytest
+import unittest
 import torch
 import torch.nn as nn
 
@@ -10,6 +11,7 @@ from float8_experimental.float8_linear_nots import Float8LinearNoTensorSubclass
 from torch._dynamo.testing import CompileCounterWithBackend, EagerAndRecordGraphs
 
 # Setting to unblock for calling contiguous in backwards
+is_H100 = torch.cuda.is_available() and torch.cuda.get_device_capability() >= (9, 0)
 
 
 def _test_compile_base(
@@ -42,24 +44,27 @@ def _test_compile_base(
 
 
 @pytest.mark.parametrize("fullgraph", [True])
-@pytest.mark.parametrize("emulate", [False, True])
+@pytest.mark.parametrize("emulate", [False, True] if is_H100 else [True])
 @pytest.mark.parametrize("use_subclass", [True])
 @pytest.mark.parametrize("dtype", [torch.bfloat16])
+@unittest.skipIf(not torch.cuda.is_available(), "CUDA not available")
 def test_eager_only(fullgraph, emulate: bool, use_subclass: bool, dtype: torch.dtype):
     _test_compile_base("eager", fullgraph, emulate, use_subclass, dtype)
 
 
 @pytest.mark.parametrize("fullgraph", [True])
-@pytest.mark.parametrize("emulate", [False, True])
+@pytest.mark.parametrize("emulate", [False, True] if is_H100 else [True])
 @pytest.mark.parametrize("use_subclass", [True])
 @pytest.mark.parametrize("dtype", [torch.bfloat16])
+@unittest.skipIf(not torch.cuda.is_available(), "CUDA not available")
 def test_aot_eager(fullgraph, emulate: bool, use_subclass: bool, dtype: torch.dtype):
     _test_compile_base("aot_eager", fullgraph, emulate, use_subclass, dtype)
 
 
 @pytest.mark.parametrize("fullgraph", [True])
-@pytest.mark.parametrize("emulate", [True, False])
+@pytest.mark.parametrize("emulate", [False, True] if is_H100 else [True])
 @pytest.mark.parametrize("use_subclass", [True])
+@unittest.skipIf(not torch.cuda.is_available(), "CUDA not available")
 @pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float32])
 def test_inductor(fullgraph, emulate: bool, use_subclass: bool, dtype: torch.dtype):
     _test_compile_base("inductor", fullgraph, emulate, use_subclass, dtype)
