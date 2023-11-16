c7891177404e883fa35fcb0570a45610e5840761
y-sq <shuqi.yang.98@gmail.com>
2023-11-15 16:00:27 -0800

    Always convert scale to float32

--- float8_experimental/float8_tensor.py
+++ float8_experimental/float8_tensor.py
@@ -100,7 +100,7 @@ class Float8Tensor(torch.Tensor):
             device=data.device,
         )
         self._data = data
-        self._scale = scale
+        self._scale = scale.to(torch.float32)
         self._orig_dtype = orig_dtype
         self._emulate = emulate
         return self
