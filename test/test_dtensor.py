a9c4ffe0ae542cabee304dc5991ed06b0a95f9da
Wanchao Liang <wanchaol@users.noreply.github.com>
2024-02-13 14:23:38 -0800

    support DTensor redistribute to replicate (allgather)
    This PR adds support for fp8tensor allgather and make DTensor(Shard(0))
    that holds a Float8Tensor to be able to do redistribute to replication
    
    ghstack-source-id: c61b70d3246a3a3ef241b471538f83f6729e8e62
    Pull Request resolved: https://github.com/pytorch-labs/float8_experimental/pull/210

--- test/test_dtensor.py
+++ test/test_dtensor.py
@@ -65,9 +65,37 @@ def test_scaled_mm(mesh: DeviceMesh, size=16):
         assert local_fp8_out.dtype == torch.float32
 
 
+def test_fp8_redistribute(mesh: DeviceMesh, size=16):
+    device = mesh.device_type
+    fp8_dtype = torch.float8_e4m3fn
+    world_size = mesh.size()
+
+    x_fp32 = torch.rand(size, size, device=device)
+
+    x_scale = tensor_to_scale(x_fp32, fp8_dtype).float()
+
+    x_fp8 = Float8Tensor.to_float8(x_fp32, x_scale, fp8_dtype)
+
+    dist_x_fp8 = DTensor.from_local(x_fp8, mesh, [Shard(0)], run_check=False)
+    out_dist = dist_x_fp8.redistribute(placements=[Replicate()])
+    assert out_dist.shape == (size * world_size, size)
+    assert out_dist.placements == (Replicate(),)
+    out_local = out_dist.to_local()
+    # after allgather the out shape should be replicate
+    assert out_local.shape == (size * world_size, size)
+    from torch.distributed._functional_collectives import AsyncCollectiveTensor
+
+    if isinstance(out_local, AsyncCollectiveTensor):
+        out_local = out_local.wait()
+
+    assert isinstance(out_local, Float8Tensor)
+    assert out_local._data.dtype == fp8_dtype
+
+
 if __name__ == "__main__":
     # float8 only works on CUDA H100 so we only test cuda and we follow
     # other test files to not use TestCase but instead just add the test
     # cases in the main func.
     device_mesh = setup_distributed()
     test_scaled_mm(device_mesh)
+    test_fp8_redistribute(device_mesh)
