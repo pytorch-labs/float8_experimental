d8015c7fd6a312a6ea19c214560b9237465f40e7
drisspg <drisspguessous@gmail.com>
2024-02-13 18:17:10 -0800

    remove re-casting

--- float8_experimental/float8_tensor.py
+++ float8_experimental/float8_tensor.py
@@ -54,9 +54,7 @@ class FromFloat8ConstrFunc(torch.autograd.Function):
 
     @staticmethod
     def forward(ctx, tensor):
-        return (tensor._data.to(tensor._orig_dtype) / tensor._scale).to(
-            tensor._orig_dtype
-        )
+        return tensor._data.to(tensor._orig_dtype) / tensor._scale
 
     @staticmethod
     def backward(ctx, g):
