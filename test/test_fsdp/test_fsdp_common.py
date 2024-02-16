1659a27ca4a49fd4ca3fba9258bdabc4e6f3747f
Andrew Gu <andgu@fb.com>
2024-02-16 07:34:37 -0800

    Update base for Update on "Removed `module` arg from `fsdp_pre_all_gather`"
    On the discussion of whether fp8 all-gather is viable _without_ compiling the pre-all-gather cast to fp8, this PR would add _more_ CPU overhead due to the `__torch_function__` override, making it less viable.
    
    [ghstack-poisoned]

--- test/test_fsdp/test_fsdp_common.py
+++ test/test_fsdp/test_fsdp_common.py
@@ -1,6 +1,7 @@
 import contextlib
+import functools
 
-from typing import List, Type
+from typing import List, Optional, Type
 
 import torch
 import torch.distributed as dist
@@ -12,17 +13,23 @@ from float8_experimental.float8_linear_utils import (
     swap_linear_with_float8_linear,
     sync_float8_amax_and_scale_history,
 )
-
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.distributed.fsdp.wrap import ModuleWrapPolicy
 from torch.testing._internal.distributed._tensor.common_dtensor import (
     ModelArgs,
     Transformer,
+    TransformerBlock,
 )
+
+# Synctactic sugar: require `use_orig_params=True` for compile
+FSDP = functools.partial(FSDP, use_orig_params=True)
 
 
 def init_transformer_with_fp8(
     module_cls: Type,
     *,
     checkpoint_activations: bool = False,
+    use_activation_hooks: Optional[bool] = None,
     use_fp8_all_gather: bool = False,
 ):
     torch.manual_seed(42)
@@ -36,13 +43,9 @@ def init_transformer_with_fp8(
     )
     module = Transformer(args)
     # Only dynamic linear supports activation hooks
-    use_hooks = module_cls is Float8DynamicLinear
+    use_hooks = use_activation_hooks or (module_cls is Float8DynamicLinear)
     return swap_linear_with_float8_linear(
-        module,
-        module_cls,
-        emulate=True,
-        use_activation_hooks=use_hooks,
-        use_fp8_all_gather=use_fp8_all_gather,
+        module, module_cls, emulate=True, use_activation_hooks=use_hooks
     )
 
 
@@ -66,6 +69,14 @@ def enable_pre_and_post_forward(enable: bool):
         config.enable_pre_and_post_forward = prev_value
 
 
+def apply_fsdp(transformer: Transformer):
+    return FSDP(
+        transformer,
+        auto_wrap_policy=ModuleWrapPolicy({TransformerBlock}),
+        device_id=dist.get_rank(),
+    )
+
+
 def check_parity_no_mp(
     test_cls,
     ref_model: nn.Module,
@@ -75,23 +86,20 @@ def check_parity_no_mp(
     local_inp: torch.Tensor,
     module_cls: Type,
 ):
-    with enable_amax_init(False), enable_pre_and_post_forward(
-        False
-    ) if module_cls is Float8Linear else contextlib.nullcontext():
-        for iter_idx in range(10):
-            losses: List[torch.Tensor] = []
-            for model, optim in ((ref_model, ref_optim), (fsdp_model, fsdp_optim)):
-                optim.zero_grad(set_to_none=(iter_idx % 2 == 0))
-                losses.append(model(local_inp).sum())
-                losses[-1].backward()
-                if model is ref_model:
-                    for param in model.parameters():
-                        dist.all_reduce(param.grad)
-                        param.grad.div_(dist.get_world_size())
-                if module_cls is Float8Linear:
-                    sync_float8_amax_and_scale_history(model)
-                optim.step()
-            test_cls.assertEqual(losses[0], losses[1])
+    for iter_idx in range(10):
+        losses: List[torch.Tensor] = []
+        for model, optim in ((ref_model, ref_optim), (fsdp_model, fsdp_optim)):
+            optim.zero_grad(set_to_none=(iter_idx % 2 == 0))
+            losses.append(model(local_inp).sum())
+            losses[-1].backward()
+            if model is ref_model:
+                for param in model.parameters():
+                    dist.all_reduce(param.grad)
+                    param.grad.div_(dist.get_world_size())
+            if module_cls is Float8Linear:
+                sync_float8_amax_and_scale_history(model)
+            optim.step()
+        test_cls.assertEqual(losses[0], losses[1])
 
 
 def check_parity_bf16_mp(
@@ -104,31 +112,28 @@ def check_parity_bf16_mp(
     local_inp: torch.Tensor,
     module_cls: Type,
 ):
-    with enable_amax_init(False), enable_pre_and_post_forward(
-        False
-    ) if module_cls is Float8Linear else contextlib.nullcontext():
-        for iter_idx in range(10):
-            losses: List[torch.Tensor] = []
-            for model, optim in (
-                (ref_model_bf16, ref_optim),
-                (fsdp_model, fsdp_optim),
-            ):
-                optim.zero_grad(set_to_none=(iter_idx % 2 == 0))
-                losses.append(model(local_inp).sum())
-                losses[-1].backward()
-                if model is ref_model_bf16:
-                    for param_bf16, param_fp32 in zip(
-                        ref_model_bf16.parameters(), ref_model.parameters()
-                    ):
-                        dist.all_reduce(param_bf16.grad)
-                        param_bf16.grad.div_(dist.get_world_size())
-                        param_fp32.grad = param_bf16.grad.float()
-                        param_bf16.grad = None
-                if module_cls is Float8Linear:
-                    sync_float8_amax_and_scale_history(model)
-                optim.step()
-                for param_fp32, param_bf16 in zip(
-                    ref_model.parameters(), ref_model_bf16.parameters()
+    for iter_idx in range(10):
+        losses: List[torch.Tensor] = []
+        for model, optim in (
+            (ref_model_bf16, ref_optim),
+            (fsdp_model, fsdp_optim),
+        ):
+            optim.zero_grad(set_to_none=(iter_idx % 2 == 0))
+            losses.append(model(local_inp).sum())
+            losses[-1].backward()
+            if model is ref_model_bf16:
+                for param_bf16, param_fp32 in zip(
+                    ref_model_bf16.parameters(), ref_model.parameters()
                 ):
-                    param_bf16.detach().copy_(param_fp32)
-            test_cls.assertEqual(losses[0], losses[1])
+                    dist.all_reduce(param_bf16.grad)
+                    param_bf16.grad.div_(dist.get_world_size())
+                    param_fp32.grad = param_bf16.grad.float()
+                    param_bf16.grad = None
+            if module_cls is Float8Linear:
+                sync_float8_amax_and_scale_history(model)
+            optim.step()
+            for param_fp32, param_bf16 in zip(
+                ref_model.parameters(), ref_model_bf16.parameters()
+            ):
+                param_bf16.detach().copy_(param_fp32)
+        test_cls.assertEqual(losses[0], losses[1])
