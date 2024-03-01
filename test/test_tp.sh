74dccef4d3115d5dfae357ac6b829a2f70d6e1d9
drisspg <drisspguessous@gmail.com>
2024-03-01 13:53:51 -0800

    guard test_everything.sh

--- test/test_tp.sh
+++ test/test_tp.sh
@@ -3,4 +3,9 @@
 # terminate script on first error
 set -e
 
+if python -c 'import torch;print(torch.cuda.is_available())' | grep -q "False"; then
+    echo "Skipping test_tp.sh because no CUDA devices are available."
+    return
+fi
+
 NCCL_DEBUG=WARN torchrun --nproc_per_node 2 test/test_tp.py
