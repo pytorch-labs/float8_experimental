a1cb6ef6d85b5ba9822d91e1c8fb9aa703343f06
Wanchao Liang <wanchaol@users.noreply.github.com>
2024-03-14 13:15:46 -0700

    lint

--- float8_experimental/float8_tensor.py
+++ float8_experimental/float8_tensor.py
@@ -7,9 +7,9 @@ from typing import Dict, Optional
 
 import torch
 
-from float8_experimental.float8_utils import tensor_to_amax, to_fp8_saturated
-
 import torch.distributed._functional_collectives as funcol
+
+from float8_experimental.float8_utils import tensor_to_amax, to_fp8_saturated
 from torch.distributed._tensor import DTensor
 
 aten = torch.ops.aten
